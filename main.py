7822c38ca0a364b529950218cc29f17eca9a3447
Hosei Kan <87958089+vioxin@users.noreply.github.com>
2026-02-21 21:46:56 +0900

    Initialize Discord bot with message handling

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from keep_alive import keep_alive
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print(f'ログインしました: {client.user}')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == 'こんにちは':
+        await message.channel.send('こんにちは！')
+keep_alive()
+token = os.environ['DISCORD_TOKEN']
+client.run(token)
